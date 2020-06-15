9d6c88a33b4376bbda1830774400d4d472c25068
vsaleem <vic.saleem@gmail.com>
2020-06-15 07:35:08 -0400

    Small grammar fix in Intro section.

--- src/App.js
+++ src/App.js
@@ -34,7 +34,7 @@ class App extends Component {
 							Native of a small town located in Mansfield, Louisiana, I lived and worked in the NW Louisiana area for over 10 years before moving to <strong>Baltimore, Maryland</strong> to pursue a career in <strong>Web Development.</strong>
 						<br/>
 						<br/>
-						 In my previous experience, I studied the effects trauma of children in foster care for over 3 years. This role brought me closer to my true passion: building front-end and back-end web applications that connect resources to children and families.
+						 In my previous experience, I studied the effects of trauma in children in foster care for over 3 years. This role brought me closer to my true passion: building front-end and back-end web applications that connect resources to children and families.
 						<br/>
 						<br/>
 						In the next phase of my career, I will use Web Development as a reliable key to solve communication issues and improve accessibility with the clients' needs in mind.
