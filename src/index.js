bc4c93f301d7c76cd39dc87f48da58e64695214e
vsaleem <vic.saleem@gmail.com>
2020-06-16 00:41:58 -0400

    Made changes to index.html, index.js, and project component; deleted default React app.css and index.css files.

--- src/index.js
+++ src/index.js
@@ -1,13 +1,7 @@
 import React from 'react';
 import ReactDom from 'react-dom';
 import App from './App';
-// import './assets/css/images'
 import './assets/js/main';
-// import './assets/js/util';
-// import './assets/sass/main.scss';
-// import './assets/webfonts'
-
-
 
 ReactDom.render(
     <App />,
