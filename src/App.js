9e9a7b88009833a434a9974e28adefa7e740b9db
vsaleem <vic.saleem@gmail.com>
2022-06-16 18:16:19 -0500

    Commented out files under construction; added new files (header copy.js and navigation.js)

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, { Component } from "react";
-import { BrowserRouter as Router } from "react-router-dom";
+import React, { Component } from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 // import { Container } from 'react-bootstrap';
-import "./assets/css/main.css";
-import "./assets/js/main";
-import "./assets/js/util";
-import Footer from "./Components/footer";
-import Projects from "./Components/projects";
-import Header from "./Components/header";
-import AboutMe from "./Components/intro";
-import Skills from "./Components/skills";
+import './assets/css/main.css';
+import './assets/js/main';
+import './assets/js/util';
+import Footer from './Components/footer';
+import Projects from './Components/projects';
+import Header from './Components/header';
+import HeaderCopy from './Components/header copy';
+import AboutMe from './Components/intro';
+import Skills from './Components/skills';
 // import NavBar from "./Components/navbar";
 
 class App extends Component {
@@ -17,16 +18,16 @@ class App extends Component {
 			<>
 				<Router>
 					{/* <NavBar /> */}
-					<Header />
+					<HeaderCopy />
 
 					{/* Add Container and make below components route paths */}
-					<main>
+					{/* <main>
 						<AboutMe />
 						<Skills />
 						<Projects />
 					</main>
 
-					<Footer />
+					<Footer /> */}
 				</Router>
 			</>
 		);
