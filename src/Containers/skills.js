041193487b1a2d2a8f6bba953b1f54accc736b97
vsaleem <vic.saleem@gmail.com>
2023-02-20 22:10:46 -0600

    Refactored skills component

--- src/Containers/skills.js
+++ src/Containers/skills.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React from 'react';
+import { Skills as skills } from '../utils/constants';
 
-function Skills() {
+const Skills = () => {
 	return (
 		<>
 			<section id='' className='skills-section'>
@@ -12,53 +13,16 @@ function Skills() {
 				<div className='skills-section__container'>
 					<div className='skills-list' id='logo'>
 						<ul>
-							<li className='lni lni-angle-double-left lni-8 skill-icon'>
-								<span> React</span>
-							</li>
-							<li className='lni lni-angle-double-left lni-8 skill-icon'>
-								<span> Redux</span>
-							</li>
-							<li className='lni lni-angle-double-left lni-8 skill-icon'>
-								<span> TypeScript</span>
-							</li>
-							<li className='lni lni-angle-double-left lni-8 skill-icon'>
-								<span>{` JavaScript (ES6+)`}</span>
-							</li>
-							<li className='lni lni-angle-double-left lni-8 skill-icon'>
-								<span> HTML</span>
-							</li>
-							<li className='lni lni-angle-double-left lni-8 skill-icon'>
-								<span> CSS</span>
-							</li>
-							<li className='lni lni-angle-double-left lni-8 skill-icon'>
-								<span> SASS / SCSS</span>
-							</li>
-							<li className='lni lni-angle-double-left lni-8 skill-icon'>
-								<span> Node JS</span>
-							</li>
-							<li className='lni lni-angle-double-left lni-8 skill-icon'>
-								<span> git</span>
-							</li>
-							<li className='lni lni-angle-double-left lni-8 skill-icon'>
-								<span> npm</span>
-							</li>
-							<li className='lni lni-angle-double-left lni-8 skill-icon'>
-								<span> BootStrap</span>
-							</li>
-							<li className='lni lni-angle-double-left lni-8 skill-icon'>
-								<span> MaterialUI</span>
-							</li>
-							<li className='lni lni-angle-double-left lni-8 skill-icon'>
-								<span> USWDS</span>
-							</li>
-							<li className='lni lni-angle-double-left lni-8 skill-icon'>
-								<span> Jest + React Library Unit Testing</span>
-							</li>
+							{skills.map((skill) => (
+								<li className='lni lni-angle-double-left lni-8 skill-icon'>
+									<span>{skill}</span>
+								</li>
+							))}
 						</ul>
 					</div>
 				</div>
 			</section>
 		</>
 	);
-}
+};
 export default Skills;
